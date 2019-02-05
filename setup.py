31142d6ed986ce7673b5c43a07b171586b9185f6
Vasily <tugv@1c.ru>
2019-02-05 10:23:08 +0300

    change setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python
 # coding: utf8
 
-VERSION = '0.1.3'
+VERSION = '0.1.4'
 
 import sys
 from setuptools import setup
 
 extra_args = {}
-if sys.version_info[0] >= 3:
-    raise Exceprion("This lib is only for python 2.7")
-	#extra_args['use_2to3'] = True
-
 
 setup(name='finamdownloader',
       version=VERSION,
       description='finamdownloader',
-      author='Serg Shabalin',
-      url='https://sergshabal@bitbucket.org/sergshabal/finamdownloader.git',
+      author='Serg Shabalin, Vasily Tugarinov',
+      url='https://github.com/vitug/finamdownloader',
       packages=['finamdownloader'],
       install_requires=['numpy', 'pandas'],
       **extra_args)
